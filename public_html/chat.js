dab135db5d75c7874a82e345afeea7c1f732d2a2
Deshens <mati.wolf11@gmail.com>
2021-01-22 16:13:28 -0300

    Primera update

--- public_html/chat.js
+++ public_html/chat.js
@@ -8,6 +8,8 @@ let btn = document.getElementById('send');
 let output = document.getElementById('output');
 let actions = document.getElementById('actions');
 
+var executionTime = 0;
+
 btn.addEventListener('click', function() {
     //Envio al servidor a traves de un evento llamado chat:message(puede tener cualquier nombre)
     //Los datos del mensaje y el username, los envio en un objeto porque solo se puede enviar una cosa
@@ -15,10 +17,13 @@ btn.addEventListener('click', function() {
         message: message.value,
         username: username.value
     });
+    executionTime = performance.now();
 });
 
 //Estoy escuchando al evento chat:message que sera enviado desde el servidor
 socket.on('server:message', function(data) {
+    executionTime = (performance.now() - executionTime);
+    console.log("Total execution time= " + executionTime + " ms");
     actions.innerHTML = ''; //Elimino el "Escribiendo mensaje"
     //Imprimo en pantalla dentro de una etiqueta P los datos recibidos  
     output.innerHTML += `<p>
